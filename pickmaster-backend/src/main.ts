7b14b0231bbb6c909e8dec3cc4bd4a0c334c003f
naseokhoo <nahoo1202@naver.com>
2025-10-28 02:57:12 +0900

    파이베이스이용안함 그냥 배포진행중

--- pickmaster-backend/src/main.ts
+++ pickmaster-backend/src/main.ts
@@ -3,11 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const allowedOrigins = [
+    'http://localhost:5173', // 로컬 테스트용 (Vite 기본 포트)
+    'https://YOUR_VERCEL_FRONTEND_URL.vercel.app', // Vercel 배포 주소
+  ];
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    origin: (origin, callback) => {
+      // origin이 없거나(postman 등) 허용된 목록에 있으면 허용
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // 쿠키나 인증 헤더 등 전송 허용
   });
 
   await app.listen(3000);
